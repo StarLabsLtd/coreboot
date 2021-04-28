9f9c74537ef304b761cf46841b44ccd59ece5879
Andy Pont <andy.pont@sdcsystems.com>
2021-04-28 15:55:28 +0100

    Add mainboard_silicon_init_params_late function to fsp_params.c and use it to determine Heci1 state based on me_state CMOS variable.

--- src/mainboard/starlabs/labtop/ramstage.c
+++ src/mainboard/starlabs/labtop/ramstage.c
@@ -25,21 +25,9 @@ void mainboard_silicon_init_params(FSP_S_CONFIG *params)
 	gpio_configure_pads(pads, num);
 }
 
-static void mainboard_enable(struct device *dev)
+void mainboard_silicon_init_params_late(FSP_S_CONFIG *params)
 {
-	struct device *csedev = pcidev_path_on_root(PCH_DEVFN_CSE);
-	printk(BIOS_DEBUG, "XXXXX PCI device state before: %d\n", csedev->enabled);
-	if (csedev->enabled) {
-		u8 me_state = get_int_option("me_state", 0xff);
-		printk(BIOS_DEBUG, "XXXXX CMOS me_state: %d\n", me_state);
-		if (me_state == 1) {
-			printk(BIOS_DEBUG, "Flagging that SMM should disable the ME\n");
-			csedev->enabled = 0;
-			printk(BIOS_DEBUG, "XXXXX PCI device state after: %d\n", csedev->enabled);
-		}
-	}
+	u8 me_state = get_int_option("me_state", 0);
+	printk(BIOS_DEBUG, "XXXXX CMOS me_state: %d\n", me_state);
+	params->Heci1Disabled = me_state;
 }
-
-struct chip_operations mainboard_ops = {
-        .enable_dev = mainboard_enable,
-};
