71f211d7feab5ed6ea712bc5f96d4dfea050dffb
Andy Pont <andy.pont@sdcsystems.com>
2021-04-28 09:19:45 +0100

    Tidy code so we only check CMOS option if PCI dev 16.0 has been set to on in the devicetree.cb file.

--- src/mainboard/starlabs/labtop/ramstage.c
+++ src/mainboard/starlabs/labtop/ramstage.c
@@ -27,15 +27,16 @@ void mainboard_silicon_init_params(FSP_S_CONFIG *params)
 
 static void mainboard_enable(struct device *dev)
 {
-	u8 me_state = get_int_option("me_state", 0xff);
-	printk(BIOS_DEBUG, "XXXXX CMOS me_state: %d\n", me_state);
-
-	if (me_state == 1) {
-		printk(BIOS_DEBUG, "Flagging that SMM should disable the ME\n");
-		struct device *csedev = pcidev_path_on_root(PCH_DEVFN_CSE);
-		printk(BIOS_DEBUG, "XXXXX PCI device state before: %d\n", csedev->enabled);
-		csedev->enabled = 0;
-		printk(BIOS_DEBUG, "XXXXX PCI device state after: %d\n", csedev->enabled);
+	struct device *csedev = pcidev_path_on_root(PCH_DEVFN_CSE);
+	printk(BIOS_DEBUG, "XXXXX PCI device state before: %d\n", csedev->enabled);
+	if (csedev->enabled) {
+		u8 me_state = get_int_option("me_state", 0xff);
+		printk(BIOS_DEBUG, "XXXXX CMOS me_state: %d\n", me_state);
+		if (me_state == 1) {
+			printk(BIOS_DEBUG, "Flagging that SMM should disable the ME\n");
+			csedev->enabled = 0;
+			printk(BIOS_DEBUG, "XXXXX PCI device state after: %d\n", csedev->enabled);
+		}
 	}
 }
 
