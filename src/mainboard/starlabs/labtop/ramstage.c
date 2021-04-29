f23db9e01e7fa1c3041689f96a244a3c5d1d3cb7
Sean Rhodes <sean@starlabs.systems>
2021-04-29 13:56:00 +0100

    Re-added method to disable 16.0 is me_state=1

--- src/mainboard/starlabs/labtop/ramstage.c
+++ src/mainboard/starlabs/labtop/ramstage.c
@@ -23,3 +23,17 @@ void mainboard_silicon_init_params(FSP_S_CONFIG *params)
 	pads = variant_gpio_table(&num);
 	gpio_configure_pads(pads, num);
 }
+static void mainboard_enable(struct device *dev)
+{
+	struct device *csedev = pcidev_path_on_root(PCH_DEVFN_CSE);
+	printk(BIOS_DEBUG, "XXXXX PCI device state before: %d\n", csedev->enabled);
+	if (csedev->enabled) {
+		u8 me_state = get_int_option("me_state", 0xff);
+		printk(BIOS_DEBUG, "XXXXX CMOS me_state: %d\n", me_state);
+		if (me_state == 1) {
+			printk(BIOS_DEBUG, "Flagging that SMM should disable the ME\n");
+			csedev->enabled = 0;
+			printk(BIOS_DEBUG, "XXXXX PCI device state after: %d\n", csedev->enabled);
+		}
+	}
+}
