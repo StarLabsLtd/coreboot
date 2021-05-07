60d4cce1680610b327b64cb2d2b622d7826cc857
Sean Rhodes <sean@starlabs.systems>
2021-05-07 14:44:18 +0100

    Revert debugging

--- src/soc/intel/skylake/lockdown.c
+++ src/soc/intel/skylake/lockdown.c
@@ -9,13 +9,9 @@
 
 static void lpc_lockdown_config(int chipset_lockdown)
 {
-	/* Set BIOS Interface Lock */
-	if (chipset_lockdown >= CHIPSET_LOCKDOWN_COREBOOT) {
-		lpc_set_bios_interface_lock_down();
-	}
-
-	/* Set BIOS Lock */
+	/* Set BIOS Interface Lock, BIOS Lock */
 	if (chipset_lockdown == CHIPSET_LOCKDOWN_COREBOOT) {
+		lpc_set_bios_interface_lock_down();
 		lpc_set_lock_enable();
 	}
 }
