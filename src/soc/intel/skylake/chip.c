79b8eea88e461a2445699f3aa1679f95e3aafcc7
Sean Rhodes <sean@starlabs.systems>
2021-07-12 17:01:20 +0100

    Updated

--- src/soc/intel/skylake/chip.c
+++ src/soc/intel/skylake/chip.c
@@ -19,6 +19,7 @@
 #include <intelblocks/xdci.h>
 #include <intelblocks/p2sb.h>
 #include <intelpch/lockdown.h>
+#include <limits.h>
 #include <soc/acpi.h>
 #include <soc/intel/common/vbt.h>
 #include <soc/interrupt.h>
@@ -503,12 +504,16 @@ void platform_fsp_silicon_init_params_cb(FSPS_UPD *supd)
 	params->PchIoApicBdfValid = 0;
 
 	/* Enable VT-d and X2APIC */
-	const uint8_t vtd = get_uint_option("vtd", 1);
+	const unsigned int vtd = get_uint_option("vtd", 1);
 	if (soc_is_vtd_capable()) {
 		params->VtdBaseAddress[0] = GFXVT_BASE_ADDRESS;
 		params->VtdBaseAddress[1] = VTVC0_BASE_ADDRESS;
 		params->X2ApicOptOut = 0;
-		tconfig->VtdDisable = !vtd;
+		if (vtd == UINT_MAX) {
+			tconfig->VtdDisable = 0;
+		} else {
+			tconfig->VtdDisable = !vtd;
+		}
 	}
 
 	params->PeiGraphicsPeimInit = CONFIG(RUN_FSP_GOP) && is_devfn_enabled(SA_DEVFN_IGD);
