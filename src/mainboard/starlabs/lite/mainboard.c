eea8595fb5517009853378d8efae07a339abd868
Sean Rhodes <sean@starlabs.systems>
2021-07-16 09:56:24 +0100

    mainboard/lite: Updated config files

--- src/mainboard/starlabs/lite/mainboard.c
+++ src/mainboard/starlabs/lite/mainboard.c
@@ -3,13 +3,18 @@
 #include <smbios.h>
 #include <types.h>
 #include <uuid.h>
-
+#include <option.h>
+#include <console/console.h>
+#include <device/device.h>
+#include <device/pci_def.h>
+#include <types.h>
+#include <baseboard/variants.h>
 #include <ec/starlabs/it8987/ec.h>
 
-/* Override the BIOS version using smbios_mainboard_bios_version() */
+
 const char *smbios_mainboard_bios_version(void)
 {
-	return "0";
+	return "6";
 }
 
 /* Get the Embedded Controller firmware version */
@@ -21,37 +26,31 @@ void smbios_ec_revision(uint8_t *ec_major_revision, uint8_t *ec_minor_revision)
 	*ec_minor_revision = ec_version & 0xff;
 }
 
-/* Override smbios_system_manufacturer */
 const char *smbios_system_manufacturer(void)
 {
 	return "Star Labs";
 }
 
-/* Override smbios_system_sku */
 const char *smbios_system_sku(void)
 {
-	return "I3";
+	if (CONFIG(BOARD_STARLABS_LITE_GLK))
+		return "I3";
+	else if (CONFIG(BOARD_STARLABS_LITE_APL))
+		return "I2";
+	else
+		return "Unknown";
 }
 
-/* Override smbios_mainboard_features_flags */
 u8 smbios_mainboard_feature_flags(void)
 {
-	return SMBIOS_FEATURE_FLAG_HOSTING_BOARD | SMBIOS_FEATURE_FLAG_REPLACEABLE;
+	return SMBIOS_FEATURE_FLAGS_HOSTING_BOARD | SMBIOS_FEATURE_FLAGS_REPLACEABLE;
 }
 
-/* Override smbios_mainboard_location_in_chassis */	
 const char *smbios_mainboard_location_in_chassis(void)
 {
 	return "Default";
 }
 
-/* Override smbios_mainboard_board_type */
-smbios_board_type smbios_mainboard_board_type(void)
-{
-	return SMBIOS_BOARD_TYPE_MOTHERBOARD;
-}
-
-/* Override smbios_mainboard_asset_tag */
 const char *smbios_mainboard_asset_tag(void)
 {
 	return "Default";
@@ -62,20 +61,42 @@ smbios_enclosure_type smbios_mainboard_enclosure_type(void)
 	return SMBIOS_ENCLOSURE_NOTEBOOK;
 }
 
-/* Override smbios_chassis_version */	
 const char *smbios_chassis_version(void)
 {
 	return smbios_mainboard_version();
 }
 
-/* Override smbios_chassis_serial_number */
 const char *smbios_chassis_serial_number(void)
 {
 	return smbios_mainboard_serial_number();
 }
 
-/* Override smbios_chassis_asset_tag */
-const char *smbios_chassis_asset_tag(void)
-{
-	return CONFIG_MAINBOARD_SERIAL_NUMBER;
-}
+/*
+ * TODO:
+ * __weak struct device *variant_devtree_update(void)
+ * {
+ *	return NULL;
+ * }
+ */
+
+/* Override dev tree settings based on CMOS settings */
+/*
+ * TODO:
+ * void devtree_update(void)
+ * {
+ *	struct device *nic_dev = NULL;
+ *	config_t *cfg = config_of_soc();
+ */
+	/* Perform any variant specific changes and return the nic_dev */
+/*
+ *	nic_dev = variant_devtree_update();
+ *
+ *	if (nic_dev != NULL) {
+ *		if (get_uint_option("wireless", 0) == 0)
+ *			nic_dev->enabled = 0;
+ *	}
+ *
+ *	if (get_uint_option("webcam", 0) == 0)
+ *		cfg->usb2_ports[6].enable = 0;
+ * }
+ */
