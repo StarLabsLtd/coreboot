2f122f9733d0701dbac283e9b1e5e18f58d3f851
Sean Rhodes <sean@starlabs.systems>
2023-04-14 21:25:19 +0100

    mb/starlabs/starbook: Add Raptor Lake StarBook Mk VI variant
    Tested using `edk2` from
    `github.com/starlabsltd/edk2/tree/uefipayload_202303`:
    * Windows 10
    * Ubuntu 20.04
    * MX Linux 19.4
    * Manjaro 21
    
    No known issues.
    
    https://starlabs.systems/pages/starbook-specification
    
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>
    Change-Id: I7c92bf92ab4de546c3633fae7e19a302409508ce

--- src/mainboard/starlabs/starbook/variants/rpl/devtree.c
+++ src/mainboard/starlabs/starbook/variants/rpl/devtree.c
@@ -15,33 +15,36 @@ void devtree_update(void)
 	struct soc_intel_common_config *common_config;
 	common_config = chip_get_common_soc_structure();
 
-	struct soc_power_limits_config *soc_conf_10core =
-		&cfg->power_limits_config[ADL_P_282_442_482_28W_CORE];
+	struct soc_power_limits_config *soc_conf_6core =
+		&cfg->power_limits_config[RPL_P_282_242_142_15W_CORE];
 
 	struct soc_power_limits_config *soc_conf_12core =
-		&cfg->power_limits_config[ADL_P_682_28W_CORE];
+		&cfg->power_limits_config[RPL_P_682_482_282_28W_CORE];
+
+	struct device *tbt_pci_dev = pcidev_on_root(0x07, 0);
+	struct device *tbt_dma_dev = pcidev_on_root(0x0d, 2);
 
 	/* Update PL1 & PL2 based on CMOS settings */
 	switch (get_power_profile(PP_POWER_SAVER)) {
 	case PP_POWER_SAVER:
 		disable_turbo();
-		soc_conf_10core->tdp_pl1_override	= 15;
+		soc_conf_6core->tdp_pl1_override	= 15;
 		soc_conf_12core->tdp_pl1_override	= 15;
-		soc_conf_10core->tdp_pl2_override	= 15;
+		soc_conf_6core->tdp_pl2_override	= 15;
 		soc_conf_12core->tdp_pl2_override	= 15;
 		common_config->pch_thermal_trip		= 30;
 		break;
 	case PP_BALANCED:
-		soc_conf_10core->tdp_pl1_override	= 15;
+		soc_conf_6core->tdp_pl1_override	= 15;
 		soc_conf_12core->tdp_pl1_override	= 15;
-		soc_conf_10core->tdp_pl2_override	= 25;
+		soc_conf_6core->tdp_pl2_override	= 20;
 		soc_conf_12core->tdp_pl2_override	= 25;
 		common_config->pch_thermal_trip		= 25;
 		break;
 	case PP_PERFORMANCE:
-		soc_conf_10core->tdp_pl1_override	= 28;
+		soc_conf_6core->tdp_pl1_override	= 15;
 		soc_conf_12core->tdp_pl1_override	= 28;
-		soc_conf_10core->tdp_pl2_override	= 40;
+		soc_conf_6core->tdp_pl2_override	= 25;
 		soc_conf_12core->tdp_pl2_override	= 40;
 		common_config->pch_thermal_trip		= 20;
 		break;
@@ -54,4 +57,10 @@ void devtree_update(void)
 	/* Enable/Disable Webcam based on CMOS settings */
 	if (get_uint_option("webcam", 1) == 0)
 		cfg->usb2_ports[CONFIG_CCD_PORT].enable = 0;
+
+	/* Enable/Disable Thunderbolt based on CMOS settings */
+	if (get_uint_option("thunderbolt", 1) == 0) {
+		tbt_pci_dev->enabled = 0;
+		tbt_dma_dev->enabled = 0;
+	}
 }
