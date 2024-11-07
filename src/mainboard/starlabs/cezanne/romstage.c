0b64b790efdfd52cb9fc46f2c116c1385315890e
Sean Rhodes <sean@starlabs.systems>
2024-11-07 08:21:13 +0000

    mb/starlabs/cezanne: Add new AMD CZN-based board
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

---
+++ src/mainboard/starlabs/cezanne/romstage.c
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <console/console.h>
+#include <soc/platform_descriptors.h>
+#include "soc/amd/common/block/psp/psp_def.h"
+#include <variants.h>
+
+void mb_pre_fspm(void)
+{
+	const struct soc_amd_gpio *pads;
+	size_t num;
+
+	pads = variant_gpio_table(&num);
+	gpio_configure_pads(pads, num);
+}
---
+++ src/mainboard/starlabs/cezanne/variants/starbook/Makefile.mk
@@ -0,0 +1,11 @@
+## SPDX-License-Identifier: GPL-2.0-only
+
+bootblock-y += gpio.c
+
+ramstage-y += devtree.c
+ramstage-y += gpio.c
+ramstage-y += port_descriptors.c
+
+romstage-y += gpio.c
+
+romstage-y += port_descriptors.c
