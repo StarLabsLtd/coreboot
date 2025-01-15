fc592a8216a938f9db2e83a0722861c1aca1d90e
Sean Rhodes <sean@starlabs.systems>
2025-01-15 14:13:20 +0000

    Revert "cpu/x86/topology: Simplify CPU topology initialization"
    This reverts commit 70bdd2e1fad9fe89835aab240ed4b41a02f15078.

--- src/include/cpu/x86/topology.h
+++ src/include/cpu/x86/topology.h
@@ -5,13 +5,15 @@
 
 #include <device/device.h>
 
-/*
- * Sets the topology information for the given CPU device using the bitfield descriptors
- * obtained from the CPUID leaves. Per Intel Software Developer Manual recommendation, it
- * prioritizes CPUID EAX=0x1f over CPUID EAX=0xb if available.
- *
- * If the topology information cannot be obtained from CPUID, it sets default values.
+/* Fill in the topology in struct path APIC based CPUID EAX=0xb.
+ * If leaf 0xb is not supported or is not implemented then no topology
+ * will be filled in.
  */
-void set_cpu_topology(struct device *cpu);
+void set_cpu_topology_from_leaf_b(struct device *cpu);
 
+/* Fill in the topology node ID in struct path APIC based CPUID EAX=0x1f
+ * or CPUID EAX=0xb. If those leaves aren't supported then the node ID
+ * won't be updated.
+ */
+void set_cpu_node_id_leaf_1f_b(struct device *cpu);
 #endif
