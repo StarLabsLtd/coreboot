c5ea4b579bafe771d2fb8509ef2e4ebb29582921
Sean Rhodes <sean@starlabs.systems>
2025-08-24 20:13:41 +0100

    vendorcode: Add StarLabs directory with ALC269-VB6
    Rough idea that could de-dup:
    * Verb Tables
    * Bootblock
    * CFR
    * CMOS
    * mainboard.c
    * SMBIOS
    * devtree
    
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>

---
+++ src/vendorcode/starlabs/hda/Makefile.mk
@@ -0,0 +1,4 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+ramstage-$(CONFIG_HAS_DMIC) += dmic.c
+ramstage-$(CONFIG_ALC269_VB6) += alc269_vb6.c
--- src/vendorcode/starlabs/hda/dmic.c
+++ src/vendorcode/starlabs/hda/dmic.c
@@ -5,9 +5,6 @@
 #include <option.h>
 #include <types.h>
 
-#define AZALIA_CODEC_ALC256 0x10ec0256
-#define AZALIA_CODEC_ALC269 0x10ec0269
-
 static const u32 override_verb[] = {
 	AZALIA_PIN_CFG(0, 0x12, AZALIA_PIN_CFG_NC(0)),
 };
@@ -19,7 +16,7 @@ static void disable_microphone(u8 *base)
 
 void mainboard_azalia_program_runtime_verbs(u8 *base, u32 viddid)
 {
-	if (viddid == AZALIA_CODEC_ALC256 || viddid == AZALIA_CODEC_ALC269) {
+	if (viddid == CONFIG_CODEC_ID) {
 		printk(BIOS_DEBUG, "CMOS: viddid = %08x\n", viddid);
 		if (get_uint_option("microphone", 1) == 0)
 			disable_microphone(base);
