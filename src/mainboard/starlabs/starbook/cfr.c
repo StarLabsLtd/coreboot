467fa9c57bf0d834313fe7d419a45aac7c695fcf
Sean Rhodes <sean@starlabs.systems>
2025-11-05 10:01:42 +0000

    mb/starlabs/*: Expose TME and BIOS Lock CFR options
    Change-Id: I806b8af593626dc3125435ba56ce18dfcd7f6946
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>

--- src/mainboard/starlabs/starbook/cfr.c
+++ src/mainboard/starlabs/starbook/cfr.c
@@ -82,6 +82,15 @@ static struct sm_obj_form devices = {
 	},
 };
 
+static struct sm_obj_form security = {
+	.ui_name = "Security",
+	.obj_list = (const struct sm_object *[]) {
+		&bios_lock,
+		&intel_tme,
+		NULL
+	},
+};
+
 static struct sm_obj_form pci = {
 	.ui_name = "PCI",
 	.obj_list = (const struct sm_object *[]) {
@@ -115,6 +124,7 @@ static struct sm_obj_form *sm_root[] = {
 	&power,
 	&keyboard,
 	&devices,
+	&security,
 	&pci,
 	&coreboot,
 	NULL
