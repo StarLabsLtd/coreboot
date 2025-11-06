21effb23ed257abc038801a9cc56c4feaf29959e
Matt DeVillier <matt.devillier@gmail.com>
2025-11-06 15:22:14 -0600

    CZN: fix keyboard
    Change-Id: Ie38255bf0037163e6b0da439267b1e6cea0ce83f
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/mainboard/starlabs/cezanne/mainboard.c
+++ src/mainboard/starlabs/cezanne/mainboard.c
@@ -56,3 +56,17 @@ const struct fch_irq_routing *mb_get_fch_irq_mapping(size_t *length)
 	*length = ARRAY_SIZE(fch_irq_map);
 	return fch_irq_map;
 }
+
+static void mainboard_init(struct device *dev)
+{
+	pc_keyboard_init(NO_AUX_DEVICE);
+}
+
+static void mainboard_enable(struct device *dev)
+{
+	dev->ops->init = mainboard_init;
+}
+
+struct chip_operations mainboard_ops = {
+	.enable_dev = mainboard_enable,
+};
