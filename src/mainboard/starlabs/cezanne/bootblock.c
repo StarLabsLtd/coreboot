bebe9f0a1b6b1e589450671f6fcc323a7493203a
Matt DeVillier <matt.devillier@gmail.com>
2025-11-06 15:25:18 -0600

    CZN: Simplify GPIO programming, update tables
    Change-Id: I4d64898580a1d3f967fbca9d7a406a58a3e58701
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/mainboard/starlabs/cezanne/bootblock.c
+++ src/mainboard/starlabs/cezanne/bootblock.c
@@ -2,9 +2,7 @@
 
 #include <bootblock_common.h>
 #include <amdblocks/lpc.h>
-#include <soc/espi.h>
 #include <soc/gpio.h>
-#include <timer.h>
 #include <variants.h>
 
 void bootblock_mainboard_early_init(void)
@@ -12,23 +10,9 @@ void bootblock_mainboard_early_init(void)
 	const struct soc_amd_gpio *pads;
 	size_t num;
 
-	struct stopwatch pcie_init_timeout_sw;
-
 	pads = variant_early_gpio_table(&num);
 	gpio_configure_pads(pads, num);
 
-	/* Delay 10ms for SSD initialisation */
-	stopwatch_init_usecs_expire(&pcie_init_timeout_sw, (10 * USECS_PER_MSEC));
-
-//	lpc_enable_sio_decode(LPC_SELECT_SIO_4E4F);
-//	lpc_enable_decode(DECODE_ENABLE_KBC_PORT | DECODE_ENABLE_ACPIUC_PORT);
-}
-
-void bootblock_mainboard_init(void)
-{
-	const struct soc_amd_gpio *pads;
-	size_t num;
-
-	pads = variant_bootblock_gpio_table(&num);
-	gpio_configure_pads(pads, num);
+	lpc_enable_sio_decode(LPC_SELECT_SIO_4E4F);
+	lpc_enable_decode(DECODE_ENABLE_KBC_PORT | DECODE_ENABLE_ACPIUC_PORT);
 }
