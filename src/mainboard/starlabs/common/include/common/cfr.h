129ee278e1b964eb21011a87a67fbf1a463c2d8b
Sean Rhodes <sean@starlabs.systems>
2025-11-27 16:00:32 +0000

    F2 FB

--- src/mainboard/starlabs/common/include/common/cfr.h
+++ src/mainboard/starlabs/common/include/common/cfr.h
@@ -100,7 +100,7 @@ static const struct sm_object power_profile = SM_DECLARE_ENUM({
 	.opt_name	= "power_profile",
 	.ui_name	= "Power Profile",
 	.ui_helptext	= "Select whether to maximize performance, battery life or both.",
-	.default_value	= 1,
+	.default_value	= PP_PERFORMANCE,
 	.values		= (const struct sm_enum_value[]) {
 			{ "Power Saver",	PP_POWER_SAVER	},
 			{ "Balanced",		PP_BALANCED	},
--- src/mainboard/starlabs/starfighter/cfr.c
+++ src/mainboard/starlabs/starfighter/cfr.c
@@ -11,7 +11,6 @@
 static struct sm_obj_form performance = {
 	.ui_name = "Performance",
 	.obj_list = (const struct sm_object *[]) {
-		&bluetooth_rtd3,
 		&fan_mode,
 		&power_profile,
 		NULL
