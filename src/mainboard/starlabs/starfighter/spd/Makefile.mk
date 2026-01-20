afc7bd1bc6d0db9a2b19c6ff461ebf7ccc851a8d
Sean Rhodes <sean@starlabs.systems>
2026-01-20 15:03:41 +0000

    mb/starlabs/starfighter: Make the memory speed configurable
    Let the user chose between 5500, 6400 and 7500MT/s.
    
    Change-Id: I91171f252e83e409904031109ee084115f6b3708
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>

--- src/mainboard/starlabs/starfighter/spd/Makefile.mk
+++ src/mainboard/starlabs/starfighter/spd/Makefile.mk
@@ -1,10 +1,24 @@
 ## SPDX-License-Identifier: GPL-2.0-only
 
 ifeq ($(CONFIG_BOARD_STARLABS_STARFIGHTER_RPL),y)
-SPD_SOURCES = 16gb # 13
-SPD_SOURCES += 32gb # 0
-SPD_SOURCES += 64gb # 8
+# RPL memory configs (selected by straps) with runtime speed selection.
+# CBFS index order is (per config): 5500, 6400, 7500.
+SPD_SOURCES = 16gb-5500
+SPD_SOURCES += 16gb
+SPD_SOURCES += 16gb-7500
+SPD_SOURCES += 32gb-5500
+SPD_SOURCES += 32gb
+SPD_SOURCES += 32gb-7500
+SPD_SOURCES += 64gb-5500
+SPD_SOURCES += 64gb-6400
+SPD_SOURCES += 64gb
 else
-SPD_SOURCES = 32gb-mtl # 13
-SPD_SOURCES += 64gb-mtl # 0
+# MTL memory configs (selected by straps) with runtime speed selection.
+# CBFS index order is (per config): 5500, 6400, 7500.
+SPD_SOURCES = 32gb-mtl-5500
+SPD_SOURCES += 32gb-mtl
+SPD_SOURCES += 32gb-mtl-7500
+SPD_SOURCES += 64gb-mtl-5500
+SPD_SOURCES += 64gb-mtl-6400
+SPD_SOURCES += 64gb-mtl
 endif
