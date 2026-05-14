StarLabsLtd/coreboot

commit af42906efa72e699181a058ccaf09f2dc3989831

soc/intel/alderlake: Set max Pkg C-states to Auto
This patch configures max Pkg C-state to Auto which limits the max
C-state to deep C-state

Signed-off-by: V Sowmya <v.sowmya@intel.com>
Change-Id: Iab92eaadad3f17ed8dddc4f383d6eeaab8c9ea6e
Reviewed-on: https://review.coreboot.org/c/coreboot/+/55706
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

src/soc/intel/alderlake/fsp_params.c
@@ -533,6 +533,7 @@ static void fill_fsps_misc_power_params(FSP_S_CONFIG *s_cfg,
 	s_cfg->PsOnEnable = 1;
 	/* Enable the energy efficient turbo mode */
 	s_cfg->EnergyEfficientTurbo = 1;
+	s_cfg->PkgCStateLimit = LIMIT_AUTO;
 }
 
 static void fill_fsps_irq_params(FSP_S_CONFIG *s_cfg,

src/soc/intel/alderlake/chip.h
@@ -71,6 +71,24 @@ enum fivr_voltage_supported {
 
 #define FIVR_ENABLE_ALL_SX (FIVR_ENABLE_S0i1_S0i2 | FIVR_ENABLE_S0i3 |	\
 			    FIVR_ENABLE_S3 | FIVR_ENABLE_S4 | FIVR_ENABLE_S5)
+/*
+ * The Max Pkg Cstate
+ * Values 0 - C0/C1, 1 - C2, 2 - C3, 3 - C6, 4 - C7, 5 - C7S, 6 - C8, 7 - C9, 8 - C10,
+ * 254 - CPU Default , 255 - Auto.
+ */
+enum pkgcstate_limit {
+	LIMIT_C0_C1      = 0,
+	LIMIT_C2         = 1,
+	LIMIT_C3         = 2,
+	LIMIT_C6         = 3,
+	LIMIT_C7         = 4,
+	LIMIT_C7S        = 5,
+	LIMIT_C8         = 6,
+	LIMIT_C9         = 7,
+	LIMIT_C10        = 8,
+	LIMIT_CPUDEFAULT = 254,
+	LIMIT_AUTO       = 255,
+};
 
 struct soc_intel_alderlake_config {
 

commit 6bab860b9addc48617608179b59500f6cf73846c

soc/intel/alderlake: Limit package C-state without S0ix
The ACPI C-state table caps non-S0ix platforms at C8, but FSP is
still told to use LIMIT_AUTO. On affected systems FSP can program
MSR_PKG_CST_CONFIG_CONTROL with package C10 enabled.

Linux detects HPET as dysfunctional when PC10 is enabled and disables
it, even though the OS-visible non-S0ix C-state table does not include
C10.

Keep LIMIT_AUTO for S0ix, but match the non-S0ix ACPI policy by
programming LIMIT_C8.

Fixes: commit af42906efa72 ("soc/intel/alderlake: Set max Pkg C-states to Auto")
Change-Id: I02d9e8eb692a07a3e733faf3601f2a2943e614ab
Signed-off-by: Sean Rhodes <sean@starlabs.systems>

src/soc/intel/alderlake/fsp_params.c
@@ -1055,7 +1055,7 @@ static void fill_fsps_misc_power_params(FSP_S_CONFIG *s_cfg,
 	s_cfg->Hwp = 1;
 	s_cfg->Cx = 1;
 	s_cfg->PsOnEnable = 1;
-	s_cfg->PkgCStateLimit = LIMIT_AUTO;
+	s_cfg->PkgCStateLimit = config->s0ix_enable ? LIMIT_AUTO : LIMIT_C8;
 
 	/* Set Energy Efficient Turbo mode */
 	s_cfg->EnergyEfficientTurbo = config->energy_efficient_turbo;