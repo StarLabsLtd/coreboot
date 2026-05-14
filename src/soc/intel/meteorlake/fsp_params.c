StarLabsLtd/coreboot

commit ae5ba37a5589c503e902ed769a81c91d0a525d83

soc/intel/meteorlake: Set max Pkg C-states to Auto
This patch configures max Pkg C-state to Auto which limits the max
C-state to deep C-state.

Port of commit af42906efa72 ("soc/intel/alderlake: Set max Pkg C-states
to Auto")

BUG=none
TEST=Boot to the OS on Google/Rex.

Snippet from FSP log:
[SPEW ]   PkgCStateLimit : 0xFF

Signed-off-by: Kapil Porwal <kapilporwal@google.com>
Change-Id: Ic403ab83a594b04920d5cf600432939687a2598b
Reviewed-on: https://review.coreboot.org/c/coreboot/+/71653
Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
Reviewed-by: Subrata Banik <subratabanik@google.com>

src/soc/intel/meteorlake/fsp_params.c
@@ -638,6 +638,7 @@ static void fill_fsps_misc_power_params(FSP_S_CONFIG *s_cfg,
 	s_cfg->Hwp = 1;
 	s_cfg->Cx = 1;
 	s_cfg->PsOnEnable = 1;
+	s_cfg->PkgCStateLimit = LIMIT_AUTO;
 	/* Enable the energy efficient turbo mode */
 	s_cfg->EnergyEfficientTurbo = 1;
 	s_cfg->PmcLpmS0ixSubStateEnableMask = get_supported_lpm_mask();

src/soc/intel/meteorlake/chip.h
@@ -43,6 +43,25 @@ enum ddi_port_flags {
 	DDI_ENABLE_HPD = 1 << 1,
 };
 
+/*
+ * The Max Pkg Cstate
+ * Values 0 - C0/C1, 1 - C2, 2 - C3, 3 - C6, 4 - C7, 5 - C7S, 6 - C8, 7 - C9, 8 - C10,
+ * 254 - CPU Default , 255 - Auto.
+ */
+enum pkgcstate_limit {
+	LIMIT_C0_C1		= 0,
+	LIMIT_C2		= 1,
+	LIMIT_C3		= 2,
+	LIMIT_C6		= 3,
+	LIMIT_C7		= 4,
+	LIMIT_C7S		= 5,
+	LIMIT_C8		= 6,
+	LIMIT_C9		= 7,
+	LIMIT_C10		= 8,
+	LIMIT_CPUDEFAULT	= 254,
+	LIMIT_AUTO		= 255,
+};
+
 /* Bit values for use in LpmStateEnableMask. */
 enum lpm_state_mask {
 	LPM_S0i2_0 = BIT(0),

commit 5f0b9e1c9797ac43fcc86a3216aa6b2710fece2b

soc/intel/meteorlake: Limit package C-state without S0ix
The ACPI C-state table caps non-S0ix platforms at C8, but FSP is
still told to use LIMIT_AUTO. On affected systems FSP can program
MSR_PKG_CST_CONFIG_CONTROL with package C10 enabled.

Linux detects HPET as dysfunctional when PC10 is enabled and disables
it, even though the OS-visible non-S0ix C-state table does not include
C10.

Keep LIMIT_AUTO for S0ix, but match the non-S0ix ACPI policy by
programming LIMIT_C8.

Fixes: commit ae5ba37a5589 ("soc/intel/meteorlake: Set max Pkg C-states to Auto")
Change-Id: Ic6774c709a891595d6e26402d14535bfb61b6ed9
Signed-off-by: Sean Rhodes <sean@starlabs.systems>

src/soc/intel/meteorlake/fsp_params.c
@@ -659,7 +659,7 @@ static void fill_fsps_misc_power_params(FSP_S_CONFIG *s_cfg,
 	s_cfg->Hwp = 1;
 	s_cfg->Cx = 1;
 	s_cfg->PsOnEnable = 1;
-	s_cfg->PkgCStateLimit = LIMIT_AUTO;
+	s_cfg->PkgCStateLimit = config->s0ix_enable ? LIMIT_AUTO : LIMIT_C8;
 	/* Enable the energy efficient turbo mode */
 	s_cfg->EnergyEfficientTurbo = 1;
 	s_cfg->PmcLpmS0ixSubStateEnableMask = get_supported_lpm_mask();