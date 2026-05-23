9b821a6b68975b31ff838b7e5da6bbebf8ca9a56
Sean Rhodes <sean@starlabs.systems>
2026-05-23 15:23:31 +0100

    mb/starlabs/lite/glk: Configure WLAN PCIe wake
    The schematic routes WLAN_PCIE_WAKE# to GPIO_116/PCIE_WAKE0_B. Configure
    the pad in its native PCIe wake function instead of leaving it as not
    connected.
    
    Change-Id: Iec420321dfb0def73f694a5ae6c26f49720040fc
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>

--- src/mainboard/starlabs/lite/variants/glk/gpio.c
+++ src/mainboard/starlabs/lite/variants/glk/gpio.c
@@ -50,6 +50,7 @@ const struct pad_config gpio_table[] = {
 	/* Wireless */
 	PAD_CFG_GPO_IOSSTATE_IOSTERM(GPIO_33, 1, DEEP, UP_20K, TxLASTRxE, DISPUPD),             /* Bluetooth RF Kill */
 	PAD_CFG_GPO_IOSSTATE_IOSTERM(GPIO_34, 1, DEEP, NONE, IGNORE, DISPUPD),                  /* WiFi RF Kill */
+	PAD_CFG_NF_IOSTANDBY_IGNORE(GPIO_116, UP_20K, DEEP, NF1),                               /* PCIe Wake */
 	PAD_CFG_NF_IOSSTATE_IOSTERM(GPIO_120, UP_20K, DEEP, NF1, HIZCRx1, ENPU),                /* Clock Request */
 	PAD_CFG_NF_IOSTANDBY_IGNORE(GPIO_192, UP_20K, DEEP, NF1),                               /* BRI Response */
 	PAD_CFG_NF_IOSTANDBY_IGNORE(GPIO_193, NONE, DEEP, NF1),                                 /* RGI Data */
@@ -178,7 +179,6 @@ const struct pad_config gpio_table[] = {
 	PAD_NC(GPIO_111, DN_20K),
 	PAD_NC(GPIO_112, DN_20K),
 	PAD_NC(GPIO_113, DN_20K),
-	PAD_NC(GPIO_116, DN_20K),
 	PAD_NC(GPIO_117, DN_20K),
 	PAD_NC(GPIO_118, DN_20K),
 	PAD_NC(GPIO_119, DN_20K),
