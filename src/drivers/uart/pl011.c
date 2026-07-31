dc3c6ae067f5f59394fadeebb3e380262494908b
Sean Rhodes <sean@starlabs.systems>
2026-07-31 08:25:53 +0100

    payloads/edk2: Remove support for UPL
    UPL was being chased for edk2's "quick path", which turned out to be
    the exact path we are taking now. Given that the handoff is slower by
    about 70ms, scrap UPL and stick with our old school tables.
    
    The previous patches can be used for reference if anyone finds an actual
    use for UPL.
    
    Change-Id: I26431391a7e1c7d29aae777eb17b200050ddd6a8
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>

--- src/drivers/uart/pl011.c
+++ src/drivers/uart/pl011.c
@@ -2,8 +2,6 @@
 
 #include <device/mmio.h>
 #include <boot/coreboot_tables.h>
-#include <boot/upl_fdt_table.h>
-#include <console/console.h>
 #include <console/uart.h>
 #include <drivers/uart/pl011.h>
 #include <stdlib.h>
@@ -79,25 +77,4 @@ enum cb_err fill_lb_serial(struct lb_serial *serial)
 	serial->input_hertz = uart_platform_refclk();
 
 	return CB_SUCCESS;
-}
-
-const char *upl_fdt_add_serial(struct device_tree_node *parent_node)
-{
-	static const char *serial_path[] = { "serial", NULL };
-	u32 addr_cells = 0, size_cells = 0;
-	struct device_tree_node *uart_node = dt_find_node(parent_node, serial_path, &addr_cells, &size_cells, 1);
-	if (!uart_node) {
-		printk(BIOS_ERR, "%s(): could not add serial node\n", __func__);
-		return NULL;
-	}
-
-	u64 reg_addrs[] = { uart_platform_base(CONFIG_UART_FOR_CONSOLE) };
-	u64 reg_sizes[] = { 0x1000 };
-	dt_add_reg_prop(uart_node, reg_addrs, reg_sizes, 1, addr_cells, size_cells);
-	dt_add_string_prop(uart_node, "compatible", "arm,pl011");
-	dt_add_u32_prop(uart_node, "clock-frequency", uart_platform_refclk());
-	dt_add_u32_prop(uart_node, "current-speed", get_uart_baudrate());
-	dt_add_u32_prop(uart_node, "reg-io-width", sizeof(uint32_t));
-
-	return "/serial";
 }
