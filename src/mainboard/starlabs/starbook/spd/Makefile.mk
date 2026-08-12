7daa2ff859413e02a167c430faf18450e9ec1c2c
Sean Rhodes <sean@starlabs.systems>
2026-08-12 10:41:24 +0100

    mb/starlabs/starbook/cml: Use production memory layout
    LabTop Mk IV production boards use dual-channel Micron
    MT40A1G16KD memory. Remove the unused memory-ID selection and its
    placeholder SPD entries, then configure both populated channels from
    the single CBFS SPD.
    
    Change-Id: Iabc9ccc7d4c2e1dbaec314e526a10a2d21ddefa6
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>

--- src/mainboard/starlabs/starbook/spd/Makefile.mk
+++ src/mainboard/starlabs/starbook/spd/Makefile.mk
@@ -7,23 +7,7 @@ SPD_SOURCES = 32gb-5500
 SPD_SOURCES += 32gb-6400
 SPD_SOURCES += 32gb-7500
 else
-# Schematics for this platform show Samsung K4A8G165WB-BCRC devices
-# which are 8Gb, 2400Mbps 512Mx16 devices.
-#
-# The hardware platforms used for testing are fitted with a Micron part
-# which has the FBGA identifier "D9ZFW".  The identification tool at
-# https://www.micron.com/support/tools-and-utilities/fbga identifies
-# this as the MT40A1G16KD-062E:E.  These are 16Gb, 1Gx16 devices.
-#
-# We have defined both SPD options below.
-SPD_SOURCES = empty_ddr4			# 0b0000
-SPD_SOURCES += micron-MT40A1G16KD-062E-E	# 0b0001
-SPD_SOURCES += empty_ddr4			# 0b0010
-SPD_SOURCES += empty_ddr4			# 0b0011
-SPD_SOURCES += empty_ddr4			# 0b0100
-SPD_SOURCES += empty_ddr4			# 0b0101
-SPD_SOURCES += samsung-K4A8G165WB-BCRC		# 0b0110
-SPD_SOURCES += samsung-K4A8G165WB-BCRC		# 0b0111
+SPD_SOURCES = micron-MT40A1G16KD-062E-E	# 0b0000
 
 LIB_SPD_DEPS = $(foreach f, $(SPD_SOURCES), src/mainboard/$(MAINBOARDDIR)/spd/$(f).spd.hex)
 endif
