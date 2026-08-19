8436abba4fd967f0d3e9845c4f89df9a7173ab7f
Sean Rhodes <sean@starlabs.systems>
2026-08-19 09:31:47 +0100

    payloads: pin final reviewed CDK2 stack
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>

--- tests/cdk2-external-test.sh
+++ tests/cdk2-external-test.sh
@@ -98,7 +98,7 @@ for board in config.emulation_qemu_x86_q35_smm_tseg config.starlabs_starbook_mtl
 		GENERIC_LINEAR_FRAMEBUFFER SMMSTORE; do
 		grep -qx "CONFIG_${symbol}=y" "$tmp/$board"
 	done
-	grep -qx 'CONFIG_CDK2_SOURCE_REVISION="dca5f794ebc6d6e130c46985022da6cc0c566501"' \
+	grep -qx 'CONFIG_CDK2_SOURCE_REVISION="43385fa7f726b1d776f00c73fad420bb46691fc0"' \
 		"$tmp/$board"
 	grep -qx 'CONFIG_CDK2_RETAINED_FV_SHA256="ca1ebfd0ff6c7c82935a4302c1ddc4cc418ed177756c678260dfb09527e1f50e"' \
 		"$tmp/$board"
