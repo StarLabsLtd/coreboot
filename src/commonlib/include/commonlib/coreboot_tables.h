7bb8beaf72887019d52eda92c7bb2bb18af7b8ab
Sean Rhodes <sean@starlabs.systems>
2026-08-27 15:26:52 +0100

    commonlib: Add CFR settings service ABI
    The Linux CFR firmware-attributes driver needs a firmware-owned way to
    discover which numeric settings can be changed and where to submit a
    transaction. Reusing variable names or accepting caller-selected SMM
    pointers would make that interface board-specific and unsafe.
    
    Define a versioned option-access child record, a fixed-size transaction
    mailbox, and a coreboot-table record that publishes the firmware-owned
    mailbox address and APM command. Tokens identify allowlisted settings;
    they are not credentials.
    
    Link: https://patchew.org/linux/cover.1787003659.git.sean@starlabs.systems/
    
    Change-Id: I1106c81fc45ef81f51af4474484f9c570d4d0932
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>

--- src/commonlib/include/commonlib/coreboot_tables.h
+++ src/commonlib/include/commonlib/coreboot_tables.h
@@ -98,6 +98,8 @@ enum {
 	LB_TAG_PANEL_POWEROFF		= 0x0049,
 	LB_TAG_SDHCI_NONPCI		= 0x004a,
 	LB_TAG_BOOT_REASON		= 0x004b,
+	/* 0x004b is used by LB_TAG_BOOT_REASON. */
+	LB_TAG_CFR_SETTINGS		= 0x004c,
 	/* The following options are CMOS-related */
 	LB_TAG_CMOS_OPTION_TABLE	= 0x00c8,
 	LB_TAG_OPTION			= 0x00c9,
@@ -709,6 +711,18 @@ struct lb_cfr {
 	uint32_t version;
 	uint32_t checksum;	/* Checksum of the variable payload. */
 	/* struct lb_cfr_option_form		forms[] */
+};
+
+/* Atomic CFR settings service record. */
+#define LB_CFR_SETTINGS_VERSION 1
+struct lb_cfr_settings {
+	uint32_t tag;
+	uint32_t size;
+	uint32_t version;
+	lb_uint64_t mailbox_address;
+	uint32_t mailbox_size;
+	uint8_t apm_cmd;
+	uint8_t reserved[3];
 };
 
 struct lb_panel_poweroff {
