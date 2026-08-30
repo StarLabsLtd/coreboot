607d291c0d3de8cf99bc7077cde71d7137831b9c
Sean Rhodes <sean@starlabs.systems>
2026-08-30 08:15:14 +0100

    drivers/spi: support latest-boot flash console
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>

--- tests/lib/flashconsole_lazy_standalone_test.sh
+++ tests/lib/flashconsole_lazy_standalone_test.sh
@@ -15,4 +15,16 @@ cc -std=gnu11 -Wall -Wextra -Werror -Wno-unused-parameter -Wno-sign-compare \
 	-I"$root/build" "$root/tests/lib/flashconsole_lazy_standalone_test.c" \
 	-o "$temporary/flashconsole-lazy-test"
 "$temporary/flashconsole-lazy-test"
+sed 's/^#define CONFIG_CONSOLE_SPI_FLASH_LATEST_BOOT 0$/#define CONFIG_CONSOLE_SPI_FLASH_LATEST_BOOT 1/' \
+	"$root/build/config.h" >"$temporary/config.h"
+cc -std=gnu11 -Wall -Wextra -Werror -Wno-unused-parameter -Wno-sign-compare \
+	-D__TEST__ -D__COREBOOT__ -D__RAMSTAGE__ -DTEST_LATEST_BOOT \
+	-fno-builtin \
+	-include "$root/src/include/kconfig.h" -include "$root/src/include/rules.h" \
+	-include "$root/src/commonlib/bsd/include/commonlib/bsd/compiler.h" \
+	-I"$temporary" -I"$root/src" -I"$root/src/include" -I"$root/src/commonlib/include" \
+	-I"$root/src/commonlib/bsd/include" -I"$root/src/arch/x86/include" \
+	-I"$root/build" "$root/tests/lib/flashconsole_lazy_standalone_test.c" \
+	-o "$temporary/flashconsole-latest-boot-test"
+"$temporary/flashconsole-latest-boot-test"
 printf '%s\n' 'flashconsole lazy standalone tests: PASS'
