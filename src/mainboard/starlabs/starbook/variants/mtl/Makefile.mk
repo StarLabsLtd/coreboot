a987c57ca70fc168e814c4a47ae3120053616ae5
Sean Rhodes <sean@starlabs.systems>
2026-09-21 15:46:20 +0100

    mb/starlabs/starbook_mtl: Reserve capsule buffers
    Signed-off-by: Sean Rhodes <sean@starlabs.systems>

--- src/mainboard/starlabs/starbook/variants/mtl/Makefile.mk
+++ src/mainboard/starlabs/starbook/variants/mtl/Makefile.mk
@@ -3,8 +3,10 @@
 bootblock-y += gpio.c
 
 romstage-y += romstage.c
+romstage-$(CONFIG_CAPSULE_BROKER_CBMEM_BUFFERS) += capsule_broker.c
 
 ramstage-y += devtree.c
 ramstage-y += gpio.c
 ramstage-y += hda_verb.c
 ramstage-y += ramstage.c
+ramstage-$(CONFIG_CAPSULE_BROKER_CBMEM_BUFFERS) += capsule_broker.c
